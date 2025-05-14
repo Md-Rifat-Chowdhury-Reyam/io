5090b9390b7fa70fc2662ea4652704c34fa338b7
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-14 21:21:45 +0600

    bufferedReader

--- src/ScannerVSBufferedReader.java
+++ src/ScannerVSBufferedReader.java
@@ -1,7 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ScannerVSBufferedReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         //Scanner
         Scanner scan = new Scanner(System.in);
@@ -13,6 +15,19 @@ public class ScannerVSBufferedReader {
 
         System.out.println("Name is : " + name);
         System.out.println("Gender is : " + gender);
+
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("your name : ");
+        String BName = br.readLine();
+
+        System.out.println("Your sex : ");
+        char sex = (char)br.read(); //The read() method of BufferedReader returns an integer (the Unicode value of the character), not a String,
+
+        System.out.println("Name : " + BName +  " sex : " + sex );
+
 
 
     }
